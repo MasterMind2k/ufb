24e783d0a2884ae3c8dc35e5bff6b2b0db9a3356
gregor.kalisnik <gregor.kalisnik@3c0c6b30-2877-11df-97a4-e9f47260eb65>
2010-03-14 19:56:28 +0000

    Added first lights stub.

---
+++ trunk/bge/src/scene/light.h
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Gregor Kališnik <gregor@unimatrix-one.org>      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License version 3        *
+ *   as published by the Free Software Foundation.                         *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ ***************************************************************************/
+#ifndef __BGE_SCENE_LIGHT_H
+#define __BGE_SCENE_LIGHT_H
+
+#include "scene/sceneobject.h"
+
+namespace BGE {
+namespace Scene {
+
+class Light : public BGE::Scene::SceneObject
+{
+  public:
+    Light();
+};
+
+}
+}
+
+#endif
