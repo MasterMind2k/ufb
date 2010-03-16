9d1d5df39125e76c03ab300af8cd998c0a4226eb
gregor.kalisnik <gregor.kalisnik@3c0c6b30-2877-11df-97a4-e9f47260eb65>
2010-03-16 14:14:54 +0000

    Some whitespace removals :D

--- trunk/bge/test/camera/movingcube.h
+++ trunk/bge/test/camera/movingcube.h
@@ -10,7 +10,6 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  ***************************************************************************/
-
 #ifndef MOVINGCUBE_H
 #define MOVINGCUBE_H
 
