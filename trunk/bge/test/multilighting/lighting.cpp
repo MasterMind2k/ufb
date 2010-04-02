ce22ea3b32619a4a49e0d7569ab72e6fd7a7a80c
gregor.kalisnik <gregor.kalisnik@3c0c6b30-2877-11df-97a4-e9f47260eb65>
2010-04-02 07:24:56 +0000

    Added moving lights to multilighting test.

---
+++ trunk/bge/test/multilighting/lighting.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Gregor Kališnik <gregor@unimatrix-one.org>      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License version 3        *
+ *   as published by the Free Software Foundation.                         *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ ***************************************************************************/
+#include "lighting.h"
+
+#include <ctime>
+
+#include <QtCore/QTimer>
+
+#include "canvas.h"
+
+#include "scene/light.h"
+
+#include "storage/storagemanager.h"
+#include "storage/mesh.h"
+#include "storage/material.h"
+#include "storage/shaderprogram.h"
+
+Lighting::Lighting()
+  : QObject(), BGE::Scene::Object()
+{
+  BGE::Storage::Material *material = BGE::Storage::StorageManager::self()->get<BGE::Storage::Material*>("/materials/Light");
+  // Setup lights
+  for (quint16 i = 0; i < 10; i ++) {
+    QString lightName = "Light_" + QString::number(i);
+    addChild(BGE::Canvas::canvas()->createLight(lightName));
+    BGE::Canvas::canvas()->light(lightName)->setQuadraticAttenuation(0.0005);
+    BGE::Canvas::canvas()->light(lightName)->move(qrand() % 500 - 300, 50, qrand() % 500 - 300);
+    BGE::Canvas::canvas()->light(lightName)->setMesh(BGE::Storage::StorageManager::self()->get<BGE::Storage::Mesh*>("/models/Cube"));
+    BGE::Canvas::canvas()->light(lightName)->setShaderProgram(BGE::Storage::StorageManager::self()->get<BGE::Storage::ShaderProgram*>("/shaders/Light"));
+    BGE::Canvas::canvas()->light(lightName)->addMaterial(material);
+  }
+
+  qsrand(time(0l));
+
+  // Init directions
+  setDirections();
+
+  QTimer *timer = new QTimer(this);
+  timer->setInterval(2000);
+  timer->setSingleShot(false);
+  timer->start();
+
+  connect(timer, SIGNAL(timeout()), SLOT(setDirections()));
+}
+
+void Lighting::calculateTransforms(qint32 timeDiff)
+{
+  /*quint32 i = 0;
+  foreach (BGE::Scene::Object *light, BGE::Scene::Object::children()) {
+    Vector3f direction = m_directions.at(i);
+    if (light->globalPosition().x() >= 300 || light->globalPosition().x() <= -300)
+      direction = direction.cwise() * Vector3f(-1, 0, 0);
+
+    if (light->globalPosition().z() >= 300 || light->globalPosition().z() <= -300)
+      direction = direction.cwise() * Vector3f(0, 0, -1);
+
+    m_directions[i++] = direction;
+    light->move(direction * (qreal) timeDiff / 1000.0);
+  }*/
+}
+
+void Lighting::setDirections()
+{
+  quint32 size = BGE::Canvas::canvas()->lights().size();
+  m_directions.clear();
+  for (quint32 i = 0; i < size; i++)
+    m_directions << Vector3f(qrand() % 100 - 50, 0, qrand() % 100 - 50);
+}
