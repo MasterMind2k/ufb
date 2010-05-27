98f7ec95860efc346bfb5f551db01d0b62bdec11
gregor.kalisnik <gregor.kalisnik@3c0c6b30-2877-11df-97a4-e9f47260eb65>
2010-05-27 13:09:51 +0000

    Removed repeat on rendering textures.

--- trunk/bge/src/driver/gl3.cpp
+++ trunk/bge/src/driver/gl3.cpp
@@ -91,6 +91,8 @@ class FBO
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, m_size.width(), m_size.height(), 0, GL_RGBA, GL_FLOAT, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, m_textures[i], 0);
       }
 
